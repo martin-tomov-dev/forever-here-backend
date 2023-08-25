331c5f9df4c3f27d12b25445295777edfed249ce
Martin Tomov <devsonspree@gmail.com>
2023-08-25 08:04:18 +0100

    add the initial message

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,6 +9,43 @@ exports.sendMessage = async (req, res, next) => {
   const StatusCodes = require("http-status-codes");
 
   const { email, phone_number } = req.body;
+  try {
+    const nodemailer = require("nodemailer");
+    // set the mail service
+    const transporter = nodemailer.createTransport({
+      host: "mail.smtp2go.com",
+      port: 2525,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    // send email
+    await transporter.sendMail({
+      from: "devsonspree@gmail.com",
+      to: email,
+      subject: "Forever Message",
+      html: "hiii!!!",
+    });
+  } catch (error) {
+    console.log("could not send");
+    return next({
+      status: StatusCodes.default.BAD_REQUEST,
+      message: `Could not send the request`,
+    });
+  }
+
+  // send sms
+  try {
+    await sms.sendSMS(phone_number, `This is SMS message!`);
+  } catch (error) {
+    console.log("sms error------>", error);
+    return next({
+      status: StatusCodes.default.INTERNAL_SERVER_ERROR,
+      message: "There were some problem to send SMS",
+    });
+  }
 
   const jobHandler = async () => {
     const { email, phone_number } = req.body;
@@ -52,9 +89,8 @@ exports.sendMessage = async (req, res, next) => {
       });
     }
   };
+  res.status(StatusCodes.default.OK).json("Please check the email box");
 
   const job = schedule.scheduleJob("*/5 * * * *", jobHandler);
   job.schedule();
-
-  res.status(StatusCodes.default.OK).json("Please check the email box");
 };
