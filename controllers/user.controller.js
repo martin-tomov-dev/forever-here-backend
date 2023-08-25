73878c561181ecbbe2b95ccbbc3c80692922a78b
Martin Tomov <devsonspree@gmail.com>
2023-08-25 14:39:50 +0100

    restore

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,7 +3,6 @@ const jwtConfig = require("../config/jwt.config");
 const bcryptUtil = require("../utils/bcrypt.util");
 const jwtUtil = require("../utils/jwt.util");
 const sms = require("../lib/sms");
-const schedule = require("node-schedule");
 
 exports.sendMessage = async (req, res, next) => {
   const StatusCodes = require("http-status-codes");
@@ -11,6 +10,7 @@ exports.sendMessage = async (req, res, next) => {
   const { email, phone_number } = req.body;
   try {
     const nodemailer = require("nodemailer");
+
     // set the mail service
     const transporter = nodemailer.createTransport({
       host: "mail.smtp2go.com",
@@ -28,57 +28,7 @@ exports.sendMessage = async (req, res, next) => {
       subject: "Forever Message",
       html: "hiii!!!",
     });
-  } catch (error) {
-    console.log("could not send");
-    return next({
-      status: StatusCodes.default.BAD_REQUEST,
-      message: `Could not send the request`,
-    });
-  }
 
-  // send sms
-  try {
-    await sms.sendSMS(phone_number, `This is SMS message!`);
-  } catch (error) {
-    console.log("sms error------>", error);
-    return next({
-      status: StatusCodes.default.INTERNAL_SERVER_ERROR,
-      message: "There were some problem to send SMS",
-    });
-  }
-
-  const jobHandler = async () => {
-    const { email, phone_number } = req.body;
-    console.log(email);
-
-    try {
-      const nodemailer = require("nodemailer");
-      // set the mail service
-      const transporter = nodemailer.createTransport({
-        host: "mail.smtp2go.com",
-        port: 2525,
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS,
-        },
-      });
-
-      // send email
-      await transporter.sendMail({
-        from: "devsonspree@gmail.com",
-        to: email,
-        subject: "Forever Message",
-        html: "hiii!!!",
-      });
-    } catch (error) {
-      console.log("could not send");
-      return next({
-        status: StatusCodes.default.BAD_REQUEST,
-        message: `Could not send the request`,
-      });
-    }
-
-    // send sms
     try {
       await sms.sendSMS(phone_number, `This is SMS message!`);
     } catch (error) {
@@ -88,9 +38,13 @@ exports.sendMessage = async (req, res, next) => {
         message: "There were some problem to send SMS",
       });
     }
-  };
-  res.status(StatusCodes.default.OK).json("Please check the email box");
 
-  // const job = schedule.scheduleJob("*/5 * * * *", jobHandler);
-  // job.schedule();
+    res.status(StatusCodes.default.OK).json("Please check the email box");
+  } catch (error) {
+    console.log("could not send");
+    return next({
+      status: StatusCodes.default.BAD_REQUEST,
+      message: `Could not send the request`,
+    });
+  }
 };
